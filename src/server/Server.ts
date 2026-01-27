4f2973dd3e21326c48b5e2e226d841ed9658dec0
Egwaldino Cassuente <egcassuente@gmail.com>
2026-01-27 17:11:03 +0000

    feat: added code style config

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,9 @@
-import express from 'express'; 
+import express from "express";
 
 const server = express();
 
-server.get('/', (_, res) => {
-    res.send('Servidor ligado!');
+server.get("/", (_, res) => {
+  res.send("Servidor ligado!");
 });
 
-export { server }; 
+export { server };
